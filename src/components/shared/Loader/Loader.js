9ef5cd16db0586a83f1b6d16ef045535a2f74904
NatalieUSA <110269066+NatalieUSA@users.noreply.github.com>
2023-03-03 08:56:43 +0200

    add Loader

--- src/components/shared/Loader/Loader.js
+++ src/components/shared/Loader/Loader.js
@@ -1,16 +1,32 @@
-import { Dna } from 'react-loader-spinner';
+// import { Dna } from 'react-loader-spinner';
 import styles from './loader.module.css';
+import { MagnifyingGlass } from 'react-loader-spinner';
 
 export const Loader = () => {
   return (
-    <Dna
-      className={styles.loader}
+    <MagnifyingGlass
       visible={true}
-      height="180"
-      width="180"
-      ariaLabel="dna-loading"
-      wrapperStyle={{}}
-      wrapperClass="dna-wrapper"
+      height="100"
+      width="100"
+      ariaLabel="MagnifyingGlass-loading"
+      wrapperStyle={{
+        height: '30vh',
+        marginRight: 'auto',
+        marginLeft: 'auto',
+        display: 'flex',
+      }}
+      wrapperClass="MagnifyingGlass-wrapper"
+      glassColor="#c0efff"
+      color="#e15b64"
     />
   );
+  // <Dna
+
+  //   visible={true}
+  //   height="180"
+  //   width="180"
+  //   ariaLabel="dna-loading"
+  //   wrapperStyle={{}}
+  //   wrapperClass="dna-wrapper"
+  // />
 };
